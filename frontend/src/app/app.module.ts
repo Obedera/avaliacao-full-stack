9a58411396b5566154fab7fe8622f7194fb62da7
Obedera <obede.silva7@gmail.com>
2023-02-10 14:23:59 -0300

    feat: adiciona projeto angular

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+
+
+
+// Set Locale PT-BR 
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt)
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
